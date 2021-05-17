58b53d90806bcaa7d7d7e5c16dbe3f678a093625
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-05-17 09:29:32 -0400

    Black formatted

--- mei2vol.py
+++ mei2vol.py
@@ -8,22 +8,22 @@ class MEI2Vol(RodanTask):
     # settings TODO
     enabled = False  # for now
     category = "Type conversion"
-    interactive = False 
+    interactive = False
 
     input_port_types = [
         {
-            'name': 'MEI',
-            'minimum': 1,
-            'maximum': 100, # magic number, let us know what the max is TODO
-            'resource_types': ['application/mei+xml'] # double check dir
+            "name": "MEI",
+            "minimum": 1,
+            "maximum": 100,  # magic number, let us know what the max is TODO
+            "resource_types": ["application/mei+xml"],  # double check dir
         }
     ]
 
     output_port_types = [
         {
-            'name': 'Volpiano',
-            'minimum': 1,
-            'maximum': 100,
-            'resource_types': ['application/text']
+            "name": "Volpiano",
+            "minimum": 1,
+            "maximum": 100,
+            "resource_types": ["application/text"],
         }
     ]
