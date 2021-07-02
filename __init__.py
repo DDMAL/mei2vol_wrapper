c5eaaabc5e673cea76e64ab50cd04063c2e5e0f9
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-07-02 08:43:54 -0400

    Renamed wrapper and updated init

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ import logging
 
 __version__ = "0.0.1"
 logger = logging.getLogger('rodan')
-module_loader('rodan.jobs.mei2vol_wrapper.mei2vol')
+module_loader('rodan.jobs.mei2vol_wrapper.m2v_wrapper')
